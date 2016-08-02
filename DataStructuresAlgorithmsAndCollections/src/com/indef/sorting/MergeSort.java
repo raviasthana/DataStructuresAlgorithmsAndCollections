d0486ef0295922922c6052337f9c4c90036e354e
raviasthana <ravi.asthana@gmail.com>
2016-08-02 12:51:29 +0100

    merge sort working - first version

--- DataStructuresAlgorithmsAndCollections/src/com/indef/sorting/MergeSort.java
+++ DataStructuresAlgorithmsAndCollections/src/com/indef/sorting/MergeSort.java
@@ -7,23 +7,65 @@ public class MergeSort {
 			return;
 		}else{
 			mergeSort(aList, 0, aList.length-1);
+			printList(aList);
 		}
 	}
 	
 	private void mergeSort(int[] aList, int start, int end){
-		System.out.println("start = " + start + ", end = " + end);
+		//System.out.println("start = " + start + ", end = " + end);
+		int[] mergedList = new int[aList.length];
 		if(start < end){
 			int mid = (start+end)/2;
 			mergeSort(aList,start,mid);
 			mergeSort(aList,mid+1,end);
-			merge(aList,start,mid,end);
+			merge(aList,mergedList,start,mid,end);
 		}
 	}
 	
-	private void merge(int[] a,int start, int mid, int end){
+	private void merge(int[] a, int[] mergedList, int start, int mid, int end){
+		//System.out.println("merge called[start = " + start + ", mid = " + mid + ", end = " + end + "]");
+		int left = start;
+		int right = mid + 1;
+		int k = start;
 		
-		System.out.println("merge called[start = " + start + ", mid = " + mid + ", end = " + end);
+		while((left <= mid) && (right <= end)){
+			if(a[left] > a[right]){
+				mergedList[k] = a[right];
+				k += 1;
+				right += 1;
+			}else{
+				mergedList[k] = a[left];
+				k += 1;
+				left += 1;
+			}
+		}
 		
+		while(left <= mid){
+			//System.out.println("k=" + k + ", left="+left);
+			mergedList[k] = a[left];
+			k += 1;
+			left += 1;
+		}
+		
+		while(right <= end){
+			mergedList[k] = a[right];
+			k += 1;
+			right += 1;
+		}
+		
+		//now mergedList[] is the sorted list at current merge level
+		//so merge it back to original list
+		while(k > start){
+			k = k-1;
+			a[k] = mergedList[k];
+		}
+	}
+	
+	private void printList(int[] list){
+		System.out.println("Printed list using Merge Sort");
+		for(int num : list){
+			System.out.println(num);
+		}
 	}
 	
 	public static void main(String[] args) {
